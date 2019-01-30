9b74b18ffa2872831eb22830578644ea729f8eb7
Zhenyu Yang <zhenyuyang@ucsb.edu>
2019-01-30 00:15:29 -0800

    added argv

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/zhenyu/badapple
+# Install script for directory: /home/zhenyu/ascii_art
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/zhenyu/badapple/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/zhenyu/ascii_art/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- test.cpp
+++ test.cpp
@@ -27,20 +27,21 @@ int main(int argc, char** argv)
     //img = imread("../../test1.png");
     //img = imread("../../test2.jpeg");
 
-    if(argc!=3)
+    if(argc!=4)
     {
 
-        cout<<"Useage: ./test <size of font> <path/to/image>"<<endl;
+        cout<<"Useage: ./test <size of font> <if show debug: 0 not show, 1 show> <path/to/image>"<<endl;
         return 1;
     }
 
-    img = imread(argv[2]);
+    img = imread(argv[3]);
 
     cvtColor(img,img,cv::COLOR_RGB2GRAY);
 
 
     int rows = img.rows;
     int cols = img.cols;
+    int ifShowDebug = atoi(argv[2]);
 
     //Mat img_text(rows,cols,CV_8UC3,Scalar(0,0,0));
     Mat img_text(rows,cols,CV_8UC3,Scalar(255, 255, 255));
@@ -51,6 +52,7 @@ int main(int argc, char** argv)
 
     for(int i = 1 ; i < rows/spacing-1;i++)
     {
+        cout<<"["<<i<<"/"<<int(rows/spacing-1)<<"]"<<endl;
         for(int j = 1 ; j < cols/spacing-1 ; j++)
         {
             Mat img_text_min = img_text.clone();
@@ -64,23 +66,24 @@ int main(int argc, char** argv)
                 //putText(img_text_temp,(string(1, c)),cvPoint(i*spacing,j*spacing),FONT_HERSHEY_SIMPLEX, 0.04f*spacing, cvScalar(255,255,255), 1, CV_AA);
                 putText(img_text_temp,(string(1, c)),cvPoint(j*spacing,i*spacing),FONT_HERSHEY_SIMPLEX, 0.04f*spacing, cvScalar(0,0,0), 1, CV_AA);
                 cvtColor(img_text_temp,img_text_temp,cv::COLOR_RGB2GRAY);
-                cout<<"img type = "<<img.type()<<endl;
-                cout<<"img_text_temp type = "<<img_text_temp.type()<<endl;
                 absdiff(img,img_text_temp,img_diff);
 
                 double error = cv::sum(img_diff)[0];
+                if(ifShowDebug!=0)
+                {
                 imshow("img",img);
                 imshow("img_text",img_text);
                 imshow("img_text_temp",img_text_temp);
                 imshow("img_diff",img_diff);
                 imshow("img_text_min",img_text_min);
                 cout<<"error = "<<error<<endl;
-                waitKey(2);
+                waitKey(1);
                 cout<<"error = "<<error<<", error_min = "<<error_min<<endl;
+                }
                 if(error_min>error)
                 {
                     error_min = error;
-                    cout<<"update img_text_min"<<endl;
+                    //cout<<"update img_text_min"<<endl;
                     cvtColor(img_text_temp,img_text_min,cv::COLOR_GRAY2RGB);
                 }
             }
@@ -88,7 +91,6 @@ int main(int argc, char** argv)
         }
     }
 
-    //putText(img_text,"a",cvPoint(30,30),FONT_HERSHEY_SIMPLEX, 0.8, cvScalar(200,200,250), 1, CV_AA);
 
     imshow("img",img);
     imshow("img_text",img_text);
